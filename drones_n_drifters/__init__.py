0366549ee1cdcf3a7d5e9f96682c76d599068d8f
thomas.roc <thomas.roc@gmail.com>
2016-06-29 15:33:05 -0300

    added folder architecture

---
+++ drones_n_drifters/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python2.7
+# encoding: utf-8
+
+__authors__ = ['Thomas Roc']
+__licence__ = 'GNU Affero GPL v3.0'
+__copyright__ = 'Copyright (c) 2016 Drones & Drifters'
---
+++ setup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python2.7
+# encoding: utf-8
+from setuptools import setup, find_packages
+from numpy.distutils.misc_util import Configuration
