38fb23178fc52886aa26f20a90e9de32c6362067
grumpynounours <thomas.roc@gmail.com>
2016-11-23 11:46:27 +0100

    Motion tracking + filtering working

--- drones_n_drifters/main.py
+++ drones_n_drifters/main.py
@@ -2,35 +2,118 @@
 # encoding: utf-8
 
 from __future__ import division
-
+import csv
 import cv2
 import skvideo.io
-
 import numpy as np
 
+#Video capture
 #cap = cv2.VideoCapture("/home/grumpynounours/Desktop/Electric_Brain/measurements/pumkin_passing_cut.avi")
 # quick fix
 cap = skvideo.io.VideoCapture("/home/grumpynounours/Desktop/Electric_Brain/measurements/pumkin_passing_test.MOV")
 
-# Define the codec and create VideoWriter object
-fps = round(cap.info['streams'][0]['nb_frames']/float(cap.info['format']['duration']))
-#out = skvideo.io.VideoWriter('/home/grumpynounours/Desktop/output.MOV', fps=fps, frameSize=(cap.width, cap.height))
-out = cv2.VideoWriter('/home/grumpynounours/Desktop/video.avi',-1,fps,(cap.width,cap.height))
-
-#color detection
+#Color detection attributes
 # RBG range value for Orange pumkin
 colorBounds = ([180, 69, 0], [255, 215, 255]) # RGB
 whiteBounds = ([245, 245, 245], [255, 255, 255])
 # Dilation kernel
 dilationKernel = np.ones((501, 501))
-
-
-# create NumPy arrays from the boundaries
+# create NumPy arrays from the color boundaries
 lower = np.array(colorBounds[0], dtype="uint8")
 upper = np.array(colorBounds[1], dtype="uint8")
 lowerW = np.array(whiteBounds[0], dtype="uint8")
 upperW = np.array(whiteBounds[1], dtype="uint8")
 
+#Motion tracking attributes
+lk_params = dict(winSize=(15, 15),  # what are those?
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+feature_params = dict(maxCorners=500,  # what are those?
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+#track_len = 10
+detect_interval = 5
+tracks = []
+frame_idx = 0
+
+# Plotting attributes
+cv2.namedWindow('test',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('test', 1200,1200)
+
+# # While loop color detection and surf masking
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret:
+#         #Color detection, here pumkin orange
+#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         maskPumkins = cv2.inRange(rgb, lower, upper)
+#         #Mask-out white
+#         # Finding white patches
+#         grays = cv2.cvtColor(rgb,cv2.COLOR_RGB2GRAY)
+#         maskWhite = cv2.inRange(rgb, lowerW, upperW)
+#         #  Dilate white patches
+#         dilatedMaskWhite = cv2.bitwise_not(cv2.dilate(maskWhite, dilationKernel))
+#         #Resulting mask
+#         mask = cv2.bitwise_and(maskPumkins, maskPumkins, mask = dilatedMaskWhite)
+#         # Plotting
+#         cv2.imshow("test", mask)
+#         cv2.waitKey(1)
+# # Release everything if job is finished
+# cap.release()
+# cv2.destroyAllWindows()
+
+# # While loop motion tracking
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret:
+#         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         vis = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
+#         if len(tracks) > 0:
+#             img0, img1 = prev_gray, frame_gray
+#             p0 = np.float32([tr[-1] for tr in tracks]).reshape(-1, 1, 2)
+#             p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
+#             p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
+#             d = abs(p0-p0r).reshape(-1, 2).max(-1)
+#             good = d < 1
+#             new_tracks = []
+#             for tr, (x, y), good_flag in zip(tracks, p1.reshape(-1, 2), good):
+#                 if not good_flag:
+#                     continue
+#                 tr.append((x, y))
+#                 #if len(tr) > track_len:
+#                 #    del tr[0]
+#                 new_tracks.append(tr)
+#                 cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
+#             tracks = new_tracks
+#             cv2.polylines(vis, [np.int32(tr) for tr in tracks], False, (0, 255, 0))
+#             #draw_str(vis, (20, 20), 'track count: %d' % len(tracks))
+#             x = 20
+#             y = 20
+#             s = 'track count: %d' % len(tracks)
+#             cv2.putText(vis, s, (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+#             cv2.putText(vis, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+#
+#         if frame_idx % detect_interval == 0:
+#             mask = np.zeros_like(frame_gray)
+#             mask[:] = 255
+#             for x, y in [np.int32(tr[-1]) for tr in tracks]:
+#                 cv2.circle(mask, (x, y), 5, 0, -1)
+#             p = cv2.goodFeaturesToTrack(frame_gray, mask = mask, **feature_params)
+#             if p is not None:
+#                 for x, y in np.float32(p).reshape(-1, 2):
+#                     tracks.append([(x, y)])
+#
+#         frame_idx += 1
+#         prev_gray = frame_gray
+#         cv2.imshow('test', vis)
+#
+#         cv2.waitKey(1)
+
+
+# While loop with both filtering and motion tracking
+# While loop color detection and surf masking
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret:
@@ -44,105 +127,161 @@ while(cap.isOpened()):
         #  Dilate white patches
         dilatedMaskWhite = cv2.bitwise_not(cv2.dilate(maskWhite, dilationKernel))
         #Resulting mask
-        mask = cv2.bitwise_and(maskPumkins, maskPumkins, mask = dilatedMaskWhite)
-        ###test###
-        # Plotting
-        cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.imshow("test", mask)
-        cv2.waitKey(1)
-        ###test###
-        #Write to video
-        print "save video..."
-        out.write(mask)
+        maskWP = cv2.bitwise_and(maskPumkins, maskPumkins, mask=dilatedMaskWhite)
+        #Motion tracking
+        frame_gray = maskWP  # cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        vis = rgb.copy()
+        if len(tracks) > 0:
+            img0, img1 = prev_gray, frame_gray
+            p0 = np.float32([tr[-1] for tr in tracks]).reshape(-1, 1, 2)
+            p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
+            p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
+            d = abs(p0-p0r).reshape(-1, 2).max(-1)
+            good = d < 1
+            new_tracks = []
+            for tr, (x, y), good_flag in zip(tracks, p1.reshape(-1, 2), good):
+                if not good_flag:
+                    continue
+                tr.append((x, y))
+                #if len(tr) > track_len:
+                #    del tr[0]
+                new_tracks.append(tr)
+                cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
+            tracks = new_tracks
+            cv2.polylines(vis, [np.int32(tr) for tr in tracks], False, (0, 255, 0))
+            #draw_str(vis, (20, 20), 'track count: %d' % len(tracks))
+            x = 20
+            y = 20
+            s = 'track count: %d' % len(tracks)
+            cv2.putText(vis, s, (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+            cv2.putText(vis, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+
+        if frame_idx % detect_interval == 0:
+            mask = np.zeros_like(frame_gray)
+            mask[:] = 255
+            for x, y in [np.int32(tr[-1]) for tr in tracks]:
+                cv2.circle(mask, (x, y), 5, 0, -1)
+            p = cv2.goodFeaturesToTrack(frame_gray, mask=mask, **feature_params)
+            if p is not None:
+                for x, y in np.float32(p).reshape(-1, 2):
+                    tracks.append([(x, y)])
+
+        frame_idx += 1
+        prev_gray = frame_gray
+        cv2.imshow('test', vis)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+# Save tracks in csv
+myfile = open('/home/grumpynounours/Desktop/Electric_Brain/measurements/captured_tracks.csv', 'wb')
+wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+wr.writerow(tracks)
+
 
 # Release everything if job is finished
 cap.release()
-out.release()
 cv2.destroyAllWindows()
 
 
-
-        #Motion capture
-        if counter == 0:
-            prvs = mask
-            hsv = np.zeros_like(mask)
-            hsv[..., 1] = 255
-            counter += 1
-        else:
-            next = mask
-            flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            hsv[..., 0] = ang * 180 / np.pi / 2
-            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-            bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            prvs = next
-            ###test###
-            # Plotting
-            cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.imshow("test", bgr)
-            cv2.waitKey(1)
-            ###test###
-
-
-
-        # Plotting
-        cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        #cv2.namedWindow('mask',cv2.WINDOW_NORMAL)
-        #cv2.imshow("image", rgb)
-        cv2.imshow("test", mask)
-        #cv2.resizeWindow('mask', 1200,1200)
-        cv2.waitKey(1)
-
-# find the colors within the specified boundaries
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-        # convert in RGB
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # the mask
-        mask = cv2.inRange(rgb, lower, upper)
-        output = cv2.bitwise_and(rgb, rgb, mask=mask)
-        # show the images
-        cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.imshow("test", np.hstack([rgb, output]))
-        cv2.waitKey(1)
-
-
-
-# motion capture
-counter = 0
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-        if counter == 0:
-            prvs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            hsv = np.zeros_like(frame)
-            hsv[..., 1] = 255
-            counter += 1
-        else:
-            next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            hsv[..., 0] = ang * 180 / np.pi / 2
-            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-            bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-            cv2.imshow('frame', bgr)
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
-            elif k == ord('s'):
-                cv2.imwrite('opticalfb.png', frame)
-                cv2.imwrite('opticalhsv.png', bgr)
-            prvs = next
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+# #Write video...does not work!
+# # Define the codec and create VideoWriter object
+# fps = round(cap.info['streams'][0]['nb_frames']/float(cap.info['format']['duration']))
+# #out = skvideo.io.VideoWriter('/home/grumpynounours/Desktop/output.MOV', fps=fps, frameSize=(cap.width, cap.height))
+# out = cv2.VideoWriter('/home/grumpynounours/Desktop/video.avi',-1,fps,(cap.width,cap.height))
+# # in while loop
+# # Write to video
+# print "save video..."
+# out.write(mask)
+# # after while loop
+# out.release()
+# cv2.destroyAllWindows()
+#
+# #Methods
+# def draw_str(dst, (x, y), s):
+#     cv2.putText(dst, s, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness = 2, lineType=cv2.CV_AA)
+#     cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.CV_AA)
+#
+#
+#
+#         #Motion capture
+#         if counter == 0:
+#             prvs = mask
+#             hsv = np.zeros_like(mask)
+#             hsv[..., 1] = 255
+#             counter += 1
+#         else:
+#             next = mask
+#             flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+#             mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+#             hsv[..., 0] = ang * 180 / np.pi / 2
+#             hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+#             bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+#             prvs = next
+#             ###test###
+#             # Plotting
+#             cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
+#             cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#             cv2.imshow("test", bgr)
+#             cv2.waitKey(1)
+#             ###test###
+#
+#
+#
+#         # Plotting
+#         cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
+#         cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#         #cv2.namedWindow('mask',cv2.WINDOW_NORMAL)
+#         #cv2.imshow("image", rgb)
+#         cv2.imshow("test", mask)
+#         #cv2.resizeWindow('mask', 1200,1200)
+#         cv2.waitKey(1)
+#
+# # find the colors within the specified boundaries
+#
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret:
+#         # convert in RGB
+#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         # the mask
+#         mask = cv2.inRange(rgb, lower, upper)
+#         output = cv2.bitwise_and(rgb, rgb, mask=mask)
+#         # show the images
+#         cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)
+#         cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#         cv2.imshow("test", np.hstack([rgb, output]))
+#         cv2.waitKey(1)
+#
+#
+#
+# # motion capture
+# counter = 0
+# while (cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret:
+#         if counter == 0:
+#             prvs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#             hsv = np.zeros_like(frame)
+#             hsv[..., 1] = 255
+#             counter += 1
+#         else:
+#             next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#             flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+#             mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+#             hsv[..., 0] = ang * 180 / np.pi / 2
+#             hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+#             bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+#
+#             cv2.imshow('frame', bgr)
+#             k = cv2.waitKey(30) & 0xff
+#             if k == 27:
+#                 break
+#             elif k == ord('s'):
+#                 cv2.imwrite('opticalfb.png', frame)
+#                 cv2.imwrite('opticalhsv.png', bgr)
+#             prvs = next
+#
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#
+# cap.release()
+# cv2.destroyAllWindows()
